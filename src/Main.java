b17b337f9164f124cb6eee04503f6589ce16d874
Nazaratunsyifa <syifazril@gmail.com>
2021-04-20 11:52:33 +0700

    operasi string

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+       String kota = "Banjarbaru";
+       int jumlah;
+       double harga;
+       char nilaiAbjad ='A';
+
+       char[] kotaArray ={'B','a','n','j','a','r','b','a','r','u'};
+
+        System.out.println(kota);
+        System.out.println(kotaArray);
+        System.out.println(kota.toLowerCase());
+        System.out.println(kota.toUpperCase());
+        System.out.println("jumlah huruf :" + kota.length());
+        System.out.println(kota.substring(3));
+        System.out.println(kota.substring(3,8));
+
+
+
+
+
     }
 }
