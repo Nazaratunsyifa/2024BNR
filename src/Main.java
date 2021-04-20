4c180dcc6d3c28fa2f642a98df7c954785e2e989
Nazaratunsyifa <syifazril@gmail.com>
2021-04-20 12:12:32 +0700

    macam macam operator

--- src/Main.java
+++ src/Main.java
@@ -17,7 +17,18 @@ public class Main {
         System.out.println(kota.substring(3));
         System.out.println(kota.substring(3,8));
 
+        int nilaiA = 10;
+        int nilaiB = 15;
 
+        System.out.println(nilaiA == nilaiB);
+        System.out.println(nilaiA != nilaiB);
+
+        nilaiA = nilaiA + 1;
+        System.out.println(nilaiA);
+        nilaiA++;
+        System.out.println(nilaiA);
+        nilaiA+=5;
+        System.out.println(nilaiA);
 
 
 
